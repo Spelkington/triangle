ff05f0980b6c367773e006fae86b1fa4df707e60
Spencer Elkington <spelkington@gmail.com>
2020-07-24 06:15:10 -0600

    pointypal able to connect to Discord

--- PointyPal/bot.py
+++ PointyPal/bot.py
@@ -4,6 +4,9 @@ import os
 import sys
 import discord
 from dotenv import load_dotenv
+import asyncio
+
+WELCOME_TEXT = open("welcome.md").read()
 
 try:
     load_dotenv()
@@ -16,6 +19,7 @@ except AssertionError as e:
     sys.exit(-1)
 
 client = discord.Client()
+guild = None
 
 @client.event
 async def on_ready():
@@ -27,5 +31,31 @@ async def on_ready():
         f'{client.user} is currently in the guild: {guild.name} (id: {guild.id})'
     )
 
+@client.event
+async def on_member_join(member):
+
+    print(f'{member.name} has joined the server! Assigning them the "unverified" status...')
+
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    role = discord.utils.get(guild.roles, name="Unverified")
+
+    print(f'Targeted role: {role.name}')
+
+    await member.add_roles(role)
+
+    channel = discord.utils.get(guild.channels, name="verification")
+
+    print(f'Printing welcome message to #{channel.name} (id: {channel.id})')
+
+    welcome_scroll = open("welcome.md").read()
+    
+
+    await asyncio.sleep(3)
+    await channel.send(welcome_scroll % (
+                member.mention,
+                discord.utils.get(guild.channels, name="rules").mention,
+                discord.utils.get(guild.members, name="Spelkington").mention,
+                discord.utils.get(guild.roles, name="Mods").mention,
+            ))
 
 client.run(TOKEN)
